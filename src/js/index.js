48f5b541a07d52f8ae6af8138d66934cc6700376
haifeng <1480408541@qq.com>
2021-03-18 11:17:52 +0800

    v1.0.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { $ } from "./jquery.js"
-import Swiper from "../../node_modules/swiper/dist/js/swiper.min.js"
+import Swiper from "../../node_modules/swiper/dist/js/swiper.js"
 //轮播图
 let mainSwiper = new Swiper('.main-banner', {
     loop: true,
